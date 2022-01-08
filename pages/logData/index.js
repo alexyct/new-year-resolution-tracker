fb7c3f261ce8bb7c5dc7285d5ee9e8c5f0c1bf2b
Leon Yao <leon.yao117@gmail.com>
2022-01-08 20:50:59 +0000

    complete log data page layout

---
+++ pages/logData/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./index.module.css";
+
+import LogData from "@/components/LogData/LogData";
+
+const index = () => {
+  return (
+    <div>
+      <LogData />
+    </div>
+  );
+};
+
+export default index;
