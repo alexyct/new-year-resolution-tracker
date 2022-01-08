ada0cbed6fe05e1bc8df4fd177791b99c3b40674
Leon Yao <leon.yao117@gmail.com>
2022-01-08 20:50:49 +0000

    complete log confirmation page

---
+++ pages/logConfirmation/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import LogConfirmation from "@/components/LogConfirmation/LogConfirmation";
+
+const index = () => {
+  return <LogConfirmation />;
+};
+
+export default index;
