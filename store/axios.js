ee7a0c22208e3548e86d10f34a3a08f903c3a254
Leon Yao <leon.yao117@gmail.com>
2022-01-09 13:33:11 +0000

    create axios instance

---
+++ store/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const instance = axios.create({
+  // baseURL: "http://157.230.214.200:3001",
+  baseURL: "https://localhost:3000",
+  // baseURL: "https://ec2-18-133-161-49.eu-west-2.compute.amazonaws.com:3000",
+  withCredentials: true,
+});
+
+instance.defaults.headers.common["Authorization"] = "AUTH TOKEN FROM INSTANCE";
+
+export default instance;
