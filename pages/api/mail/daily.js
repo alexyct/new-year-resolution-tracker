5db7f2cd49bf959e4a6a2c43d2da950cb80a9000
Alex Tung <taiwan.alex2000@gmail.com>
2022-01-10 15:25:45 +0800

    change mail api endpoint

--- pages/api/mail/daily.js
+++ pages/api/mail/daily.js
@@ -23,14 +23,8 @@ export default async function handler(req, res) {
   });
 
   try {
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-        return res.status(400).json({ sent: 'nope' });
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
     return res.status(200).json({ sent: 'ok' });
   } catch (error) {
     console.error(`Failed to send email: `, error);
