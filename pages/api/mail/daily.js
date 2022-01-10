1d3d30c6c8f734fcf3ef8b84912a82e4c7af7702
Alex Tung <taiwan.alex2000@gmail.com>
2022-01-10 11:38:06 +0800

    add email functionalities

---
+++ pages/api/mail/daily.js
@@ -0,0 +1,48 @@
+const nodemailer = require('nodemailer');
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).send({
+      error: 'Method Not Allowed',
+    });
+  }
+
+  let mailOptions = {
+    from: 'newyearresolutiontracker@gmail.com',
+    to: req.body.emailTo,
+    subject: 'Log data for your new your resolution tracking!',
+    text: 'Click here to log your data',
+    html: '<p>Click <a href="https://nyrtracker.vercel.app/logData">here</a> to log your data</p>',
+  };
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_FROM,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  try {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    return res.status(200).json({ sent: 'ok' });
+  } catch (error) {
+    console.error(`Failed to send email: `, error);
+    return res.status(400).send({
+      error: 'Could not send the email',
+    });
+  }
+  // transporter.sendMail(mailOptions, function (error, info) {
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log('Email sent: ' + info.response);
+  //     return res.status(200).json('Latest');
+  //   }
+  // });
+}
--- pages/test.js
+++ pages/test.js
@@ -1,15 +1,70 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
 
+// import { sendMail } from '@/lib/mail.js';
+
 export default function TestPage() {
   const { data: session } = useSession();
-  console.log(session);
+
+  const sendDemoEmail = async () => {
+    try {
+      const res = await fetch(`/api/mail/demo`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          emailTo: session.user.email,
+        }),
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      return;
+    }
+  };
+
+  const sendDailyEmail = async () => {
+    try {
+      const res = await fetch(`/api/mail/daily`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          emailTo: session.user.email,
+        }),
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      return;
+    }
+  };
+
   if (session) {
     return (
       <>
-        {session.user.id}
-        Signed in as {session.user.email}
-        <br />
+        <p>Signed in as {session.user.email}</p>
         <button onClick={() => signOut()}>Sign out</button>
+        <p>
+          For judges of the hackathon: we know you probably don&apos;t have a
+          week to wait for our weekly automations to send, so click this button
+          to see a demo of our weekly automated email. It takes in all the logs
+          you have made and show you a summary of your progress this week,
+          generate insights to help you better acheive your resolution, and
+          prompt you to write a memo to reflect on your progress and keep up the
+          good work.
+        </p>
+        <button onClick={() => sendDemoEmail()}>
+          Send Demo of Automated Weekly Report
+        </button>
+        <p>
+          We also have automated daily emails at 9pm to remind you to log data.
+          Click this button get an email now;
+        </p>
+        <button onClick={() => sendDailyEmail()}>
+          Send Reminder Email Now
+        </button>
       </>
     );
   }
