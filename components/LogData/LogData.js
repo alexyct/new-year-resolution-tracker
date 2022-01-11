b66c4b111968d5af79db07151a37324675f3b7c7
Leon Yao <leon.yao117@gmail.com>
2022-01-11 22:51:07 +0000

    make logdata dynamic to time and connect to backend

--- components/LogData/LogData.js
+++ components/LogData/LogData.js
@@ -1,34 +1,81 @@
-import Button from "components/Button/Button";
 import React from "react";
 import classes from "./LogData.module.css";
 
 import LogEntry from "./LogEntry/LogEntry";
+import Button from "components/Button/Button";
+
+function dateToString(date) {
+  let day;
+  if (!date) {
+    day = new Date();
+  } else {
+    day = new Date(date);
+  }
+  var dd = String(day.getDate()).padStart(2, "0");
+  var mm = String(day.getMonth() + 1).padStart(2, "0");
+  var yyyy = day.getFullYear();
+  day = mm + "/" + dd + "/" + yyyy;
+  return day;
+}
 
 const LogData = (props) => {
-  const date = "January 8th 2022";
-
-  const exerciseOptions = ["workout", "basketball", "football"];
-  const timeOptions = ["0:00", "0:15", "0:30"];
+  const exerciseOptions = [
+    "Walk",
+    "Run",
+    "Cycling",
+    "Rower",
+    "HIIT",
+    "Hiking",
+    "Yoga",
+    "Dance",
+    "Strength Training",
+    "Swimming",
+  ];
 
   let logData = (
     <div className={classes.logData}>
       <div className={classes.dateWrapper}>
-        <h1 className={classes.date}>{date}</h1>
+        <h1 className={classes.date}>{dateToString(props.date)}</h1>
       </div>
       <div className={classes.entries}>
-        <LogEntry title={"Exercise type"} options={exerciseOptions} />
-        <LogEntry title={"Start time"} options={timeOptions} />
-        <LogEntry title={"End time"} options={timeOptions} />
+        <LogEntry
+          title={"Exercise type"}
+          options={exerciseOptions}
+          changed={props.exerciseTypeChangedHandler}
+          value={props.logData.exercise}
+        />
+        <LogEntry
+          title={"Start time"}
+          changed={props.startTimeChangedHandler}
+          value={props.logData.startTime}
+        />
+        <LogEntry
+          title={"End time"}
+          changed={props.endTimeChangedHandler}
+          value={props.logData.endTime}
+        />
       </div>
       <div className={classes.buttonWrapper}>
-        <Button tertiary narrow>
+        <Button
+          color="black"
+          tertiary
+          narrow
+          clicked={props.logButtonClickedHandler}
+        >
           Log it
         </Button>
       </div>
     </div>
   );
 
-  return <div>{logData}</div>;
+  return (
+    <div>
+      <Button tertiary color="black" narrow>
+        Back
+      </Button>
+      {logData}
+    </div>
+  );
 };
 
 export default LogData;
--- pages/logData/index.js
+++ pages/logData/index.js
@@ -1,14 +1,69 @@
-import React from "react";
-import classes from "./index.module.css";
+import React, { useState } from "react";
+import axios from "@/store/axios";
+import { useSession } from "next-auth/react";
 
 import LogData from "@/components/LogData/LogData";
 
-const index = () => {
+const Index = () => {
+  const session = useSession();
+  // TODO: get date from history
+  let date = new Date();
+  date.setHours(0, 0, 0, 0);
+  const [exercise, setExercise] = useState("workout");
+  const [startTime, setStartTime] = useState("00:00");
+  const [endTime, setEndTime] = useState("00:00");
+
+  const logData = {
+    type: exercise,
+    startTime: startTime,
+    endTime: endTime,
+  };
+
+  const exerciseChangedHandler = (e) => {
+    setExercise(e.target.value);
+  };
+
+  const startTimeChangedHandler = (e) => {
+    setStartTime(e.target.value);
+  };
+
+  const endTimeChangedHandler = (e) => {
+    setEndTime(e.target.value);
+  };
+
+  const logButtonClickedHandler = () => {
+    // hour = 60 * 60 * 1000
+    let start = date;
+    start.setHours(startTime.split(":")[0], startTime.split(":")[1], 0, 0);
+    start = start.toISOString();
+    let end = date;
+    end.setHours(endTime.split(":")[0], endTime.split(":")[1], 0, 0);
+    end = end.toISOString();
+
+    // TODO: validate start < end
+    const data = { type: exercise, startDateTime: start, endDateTime: end };
+    axios
+      .post(`/api/logs${session.user.id}`, data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
-      <LogData />
+      <LogData
+        date={date}
+        logData={logData}
+        exerciseChangedHandler={exerciseChangedHandler}
+        startTimeChangedHandler={startTimeChangedHandler}
+        endTimeChangedHandler={endTimeChangedHandler}
+        logButtonClickedHandler={logButtonClickedHandler}
+      />
     </div>
   );
 };
 
-export default index;
+export default Index;
