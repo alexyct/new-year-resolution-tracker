8179e0e141e8e143a120e665d69c56aa0e801c34
Leon Yao <leon.yao117@gmail.com>
2022-01-12 14:54:26 +0000

    add error handling

--- components/Dashboard/Overview/BarGraph.js
+++ components/Dashboard/Overview/BarGraph.js
@@ -36,74 +36,70 @@ function drawVisualization() {
 }
 const BarGraph = (props) => {
   const [chart, setChart] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
   useEffect(() => {
-    console.log('redrawing')
+    setErrorMessage(null);
     if (props.google) {
-      const data = google.visualization.arrayToDataTable(props.data);
-      // const data = new google.visualization.DataTable();
-      // data.addColumn("string", "Day");
-      // data.addColumn("number", "Hours");
-      // data.addRows([
-      //   ["Monday", 3],
-      //   ["Tuesday", 1],
-      //   ["Wednesday", 1],
-      //   ["Thursday", 1],
-      //   ["Friday", 2],
-      //   ["Saturday", 2],
-      //   ["Sunday", 3],
-      // ]);
-
-      // Set chart options
-      var options = {
-        title: "Weekly Report",
-        vAxis: {
-          title: "Hours",
-          minValue: 0,
-          gridlines: { color: "#e4e4e4", count: 4 },
-          textStyle: { color: "black" },
-        },
-        bar: { groupWidth: "75%" },
-        colors: [
-          "red",
-          "grey",
-          "#63CA6E",
-          "#3CB9EE",
-          "#B472E8",
-          "#E87272",
-          "#FF9052",
-        ],
-        seriesType: "bars",
-        series: {
-          0: {
-            type: "line",
-            lineDashStyle: [4, 4],
+      let data;
+      try {
+        data = google.visualization.arrayToDataTable(props.data);
+        // Set chart options
+        var options = {
+          title: "Weekly Report",
+          vAxis: {
+            title: "Hours",
+            minValue: 0,
+            gridlines: { color: "#e4e4e4", count: 4 },
+            textStyle: { color: "black" },
           },
-          1: {
-            type: "line",
-            lineDashStyle: [4, 4],
+          bar: { groupWidth: "75%" },
+          colors: [
+            "red",
+            "grey",
+            "#63CA6E",
+            "#3CB9EE",
+            "#B472E8",
+            "#E87272",
+            "#FF9052",
+          ],
+          seriesType: "bars",
+          series: {
+            0: {
+              type: "line",
+              lineDashStyle: [4, 4],
+            },
+            1: {
+              type: "line",
+              lineDashStyle: [4, 4],
+            },
           },
-        },
-        curveType: "function",
-        isStacked: true,
-        chartArea: { width: "90%", height: "80%" },
-        animation: { startup: true },
-        legend: { position: "bottom" },
-      };
+          curveType: "function",
+          isStacked: true,
+          chartArea: { width: "90%", height: "80%" },
+          animation: { startup: true },
+          legend: { position: "bottom" },
+        };
 
-      // Instantiate and draw our chart, passing in some options.
-      const newChart = new google.visualization.ComboChart(
-        document.getElementById("chart_div")
-      );
-      newChart.draw(data, options);
-      // newChart.draw(data, google.charts.ComboChart.convertOptions(options));
+        // Instantiate and draw our chart, passing in some options.
+        const newChart = new google.visualization.ComboChart(
+          document.getElementById("chart_div")
+        );
+        newChart.draw(data, options);
+        // newChart.draw(data, google.charts.ComboChart.convertOptions(options));
 
-      setChart(newChart);
+        setChart(newChart);
+      } catch {
+        console.log("caught error in rendering graph");
+        setErrorMessage("something went wrong");
+        return;
+      }
     }
-  }, [props.google, props.width]);
-
+  }, [props.google, props.width, props.data]);
   return (
     <div>
-      <div className={classes.chart} id="chart_div"></div>
+      <div className={classes.chart} id="chart_div" />
+      <p className={classes.error}>{errorMessage}</p>
     </div>
   );
 };
