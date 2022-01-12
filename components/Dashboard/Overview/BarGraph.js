a41231d6e43cd996637e5ad15057d8ace4cd106c
Leon Yao <leon.yao117@gmail.com>
2022-01-12 16:35:56 +0000

    delete unused function

--- components/Dashboard/Overview/BarGraph.js
+++ components/Dashboard/Overview/BarGraph.js
@@ -2,38 +2,7 @@ import React, { useEffect, useState } from "react";
 import classes from "./Overview.module.css";
 
 // import { ResponsiveBar } from "@nivo/bar";
-function drawVisualization() {
-  // Some raw data (not necessarily accurate)
-  var data = google.visualization.arrayToDataTable([
-    [
-      "Month",
-      "Bolivia",
-      "Ecuador",
-      "Madagascar",
-      "Papua New Guinea",
-      "Rwanda",
-      "Average",
-    ],
-    ["2004/05", 165, 938, 522, 998, 450, 614.6],
-    ["2005/06", 135, 1120, 599, 1268, 288, 682],
-    ["2006/07", 157, 1167, 587, 807, 397, 623],
-    ["2007/08", 139, 1110, 615, 968, 215, 609.4],
-    ["2008/09", 136, 691, 629, 1026, 366, 569.6],
-  ]);
 
-  var options = {
-    title: "Monthly Coffee Production by Country",
-    vAxis: { title: "Cups" },
-    hAxis: { title: "Month" },
-    seriesType: "bars",
-    series: { 5: { type: "line" } },
-  };
-
-  var chart = new google.visualization.ComboChart(
-    document.getElementById("chart_div")
-  );
-  chart.draw(data, options);
-}
 const BarGraph = (props) => {
   const [chart, setChart] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
