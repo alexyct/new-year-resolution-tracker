cc024e38f3e520e9f15ed788aec02481f16ec27f
Leon Yao <leon.yao117@gmail.com>
2022-01-12 16:48:47 +0000

    add email api posts

--- pages/index.js
+++ pages/index.js
@@ -94,6 +94,31 @@ const Index = () => {
       // post resolution
       // const data = { resolutionData };
       setIsLoading(true);
+      // email APIs
+
+      axios
+        .post(
+          `https://www.easycron.com/rest/add?token=2c1a12733fbeb2af6d56c01638cfa846&url=https://nyrtracker.vercel.app/api/mail/daily&cron_expression=0 21 * * *&timezone_from=2&timezone=America/Los_Angeles&http_method=POST&http_headers=Content-Type: application/json&http_message_body={"emailTo": ${session.user.email}}&cron_job_name=Daily (${session.user.email})
+      `
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      axios
+        .post(
+          `https://www.easycron.com/rest/add?token=2c1a12733fbeb2af6d56c01638cfa846&url=https://nyrtracker.vercel.app/api/mail/weekly/${session.user.id}&cron_expression=1 21 * * 0&timezone_from=2&timezone=America/Los_Angeles&http_method=POST&http_headers=Content-Type: application/json&http_message_body={"name": ${session.user.name}, "emailTo": ${session.user.email}&cron_job_name=Weekly (${session.user.email})`
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
       axios
         .post(`/api/resolutions/${session.user.id}`, resolutionData)
         .then((response) => {
