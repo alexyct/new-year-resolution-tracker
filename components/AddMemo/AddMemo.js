e1aec08300913f53c0fcbccc2a5072acc7002b38
Leon Yao <leon.yao117@gmail.com>
2022-01-12 15:39:08 +0000

    add dynamic date from query, prevent invalid week

--- components/AddMemo/AddMemo.js
+++ components/AddMemo/AddMemo.js
@@ -3,11 +3,30 @@ import classes from "./AddMemo.module.css";
 import { useRouter } from "next/router";
 
 import Button from "components/Button/Button";
+import { getCurrWeek } from "pages";
 
 const AddMemo = (props) => {
   const router = useRouter();
-  let { week, startDate, endDate, saveMemoClickedHandler } = { props };
-  week = 3;
+  let { memo, week, saveMemoClickedHandler, memoChangedHandler } = { ...props };
+
+  let start = new Date(2022, 0, 3);
+  let end = new Date(2022, 0, 3);
+  if (week > 1) {
+    start.setDate(start.getDate() + (week - 1) * 7);
+    end.setDate(end.getDate() + (week - 1) * 7 + 6);
+  }
+  if (week > getCurrWeek()) {
+    router.push(`/addMemo?week=${getCurrWeek()}`);
+  }
+  start =
+    String(start.getDate()).padStart(2, "0") +
+    "/" +
+    String(start.getMonth() + 1).padStart(2, "0");
+  end =
+    String(end.getDate()).padStart(2, "0") +
+    "/" +
+    String(end.getMonth() + 1).padStart(2, "0");
+
   return (
     <div className={classes.memoBackdrop}>
       <div className={classes.backButton}>
@@ -24,7 +43,9 @@ const AddMemo = (props) => {
       </div>
       <div className={classes.addMemo}>
         <h1>Memo for Week {week}</h1>
-        <h3>11th Jan - 18th Jan</h3>
+        <h3>
+          {start} - {end}
+        </h3>
         <ul className={classes.prompts}>
           <li>How are you feeling this week?</li>
           <li>
@@ -32,7 +53,12 @@ const AddMemo = (props) => {
           </li>
           <li>What will you do next week to make sure you stay on track?</li>
         </ul>
-        <textarea className={classes.textarea} autoFocus={true}></textarea>
+        <textarea
+          className={classes.textarea}
+          autoFocus={true}
+          value={memo}
+          onChange={memoChangedHandler}
+        ></textarea>
         <div className={classes.buttonWrapper}>
           <Button
             color="white"
